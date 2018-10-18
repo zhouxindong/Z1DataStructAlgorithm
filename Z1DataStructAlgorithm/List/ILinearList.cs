38a2b72c0cd86bf47a9d003467ab516eae292ea4
zhouxindong <zhouxindong@163.com>
2018-10-18 19:02:48 +0800

    After defined ILinearList

--- Z1DataStructAlgorithm/List/ILinearList.cs
+++ Z1DataStructAlgorithm/List/ILinearList.cs
@@ -3,8 +3,15 @@
     /// <summary>
     /// interface for linear list
     /// </summary>
-    public interface ILinearList
+    public interface ILinearList<T>
     {
-        
+        int GetLength();
+        ILinearList<T> Clear();
+        bool IsEmpty();
+        ILinearList<T> Append(T item);
+        ILinearList<T> Insert(T item, int i);
+        T Delete(int i);
+        T GetElem(int i);
+        int Locate(T value);
     }
 }
