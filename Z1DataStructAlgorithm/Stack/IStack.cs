4df1c2cb53cdffb09f72c82bc938853a9f739a23
zhouxindong <zhouxindong@163.com>
2018-10-23 08:39:04 +0800

    Add IStack

---
+++ Z1DataStructAlgorithm/Stack/IStack.cs
@@ -0,0 +1,12 @@
+﻿namespace Z1DataStructAlgorithm.Stack
+{
+    public interface IStack<T>
+    {
+        int GetLength();
+        bool IsEmpty();
+        IStack<T> Clear();
+        IStack<T> Push(T item);
+        T Pop();
+        T PeekPop();
+    }
+}
