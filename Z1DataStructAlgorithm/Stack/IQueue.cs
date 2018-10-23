afdee994969b618f1ce73ffd9f4f7155adb009e2
zhouxindong <zhouxindong@163.com>
2018-10-23 15:33:29 +0800

    Added IQueue

---
+++ Z1DataStructAlgorithm/Stack/IQueue.cs
@@ -0,0 +1,12 @@
+﻿namespace Z1DataStructAlgorithm.Stack
+{
+    public interface IQueue<T>
+    {
+        int GetLength();
+        bool IsEmpty();
+        IQueue<T> Clear();
+        IQueue<T> In(T item);
+        T Out();
+        T PeekFront();
+    }
+}
