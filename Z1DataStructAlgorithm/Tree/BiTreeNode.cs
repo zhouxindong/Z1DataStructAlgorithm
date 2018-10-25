858f9b91b75b3a450d72a3afe18cf0a4b94dcd33
zhouxindong <zhouxindong@163.com>
2018-10-25 15:29:22 +0800

    Added BiTreeNode

---
+++ Z1DataStructAlgorithm/Tree/BiTreeNode.cs
@@ -0,0 +1,30 @@
+﻿namespace Z1DataStructAlgorithm.Tree
+{
+    public class BiTreeNode<T>
+    {
+        public T Data { get; set; }
+        public BiTreeNode<T> LeftChild { get; set; }
+        public BiTreeNode<T> RightChild { get; set; }
+
+        public BiTreeNode(T val, BiTreeNode<T> lp, BiTreeNode<T> rp)
+        {
+            Data = val;
+            LeftChild = lp;
+            RightChild = rp;
+        }
+
+        public BiTreeNode(BiTreeNode<T> lp, BiTreeNode<T> rp) : this(default(T), lp, rp)
+        {
+        }
+
+        public BiTreeNode(T val) : this(val, null, null)
+        {
+        }
+
+        public BiTreeNode() : this(default(T), null, null)
+        {
+        }
+
+
+    }
+}
