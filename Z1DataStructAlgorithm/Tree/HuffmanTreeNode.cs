32432c0b7773b4ee5173c72c6f272cf952791ff4
zhouxindong <zhouxindong@163.com>
2018-10-26 09:10:42 +0800

    Added HuffmanTreeNode

---
+++ Z1DataStructAlgorithm/Tree/HuffmanTreeNode.cs
@@ -0,0 +1,20 @@
+﻿namespace Z1DataStructAlgorithm.Tree
+{
+    public class HuffmanTreeNode
+    {
+        public int Weight { get; set; }
+        public int LeftChild { get; set; }
+        public int RightChild { get; set; }
+        public int Parent { get; set; }
+
+        public HuffmanTreeNode(int weight, int left_child, int right_child, int parent)
+        {
+            Weight = weight;
+            LeftChild = left_child;
+            RightChild = right_child;
+            Parent = parent;
+        }
+
+        public HuffmanTreeNode() : this(0, -1, -1, -1) { }
+    }
+}
