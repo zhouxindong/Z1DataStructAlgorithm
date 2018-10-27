bbbeb19ca202c76911d5fc04adc609820a047c73
zhouxindong <zhouxindong@163.com>
2018-10-27 22:29:28 +0800

    Add GraphNode

---
+++ Z1DataStructAlgorithm/Graph/GraphNode.cs
@@ -0,0 +1,12 @@
+﻿namespace Z1DataStructAlgorithm.Graph
+{
+    public class GraphNode<T>
+    {
+        public T Data { get; set; }
+
+        public GraphNode(T data)
+        {
+            Data = data;
+        }
+    }
+}
