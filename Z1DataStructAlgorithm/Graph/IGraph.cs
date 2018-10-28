3e9c221a6eb77303f2464f84fe9322e02f5a2fed
zhouxindong <zhouxindong@163.com>
2018-10-28 22:21:58 +0800

    Add IGraph

---
+++ Z1DataStructAlgorithm/Graph/IGraph.cs
@@ -0,0 +1,11 @@
+﻿namespace Z1DataStructAlgorithm.Graph
+{
+    public interface IGraph<T>
+    {
+        int GetNumOfVertex();
+        int GetNumOfEdge();
+        void SetEdge(GraphNode<T> v1, GraphNode<T> v2, int v);
+        void DelEdge(GraphNode<T> v1, GraphNode<T> v2);
+        bool HasEdge(GraphNode<T> v1, GraphNode<T> v2);
+    }
+}
