8e7ca9de012554711e9dca3b9d35e334e2373ab1
zhouxindong <zhouxindong@163.com>
2018-10-29 11:10:44 +0800

    Added VexNode

---
+++ Z1DataStructAlgorithm/Graph/VexNode.cs
@@ -0,0 +1,18 @@
+﻿namespace Z1DataStructAlgorithm.Graph
+{
+    public class VexNode<T>
+    {
+        public GraphNode<T> Data { get; set; }
+        public AdjListNode<T> FirstAdj { get; set; }
+
+        public VexNode(GraphNode<T> data, AdjListNode<T> first_adj)
+        {
+            Data = data;
+            FirstAdj = first_adj;
+        } 
+
+        public VexNode() : this(null, null) { }
+
+        public VexNode(GraphNode<T> data) : this(data, null) { }
+    }
+}
