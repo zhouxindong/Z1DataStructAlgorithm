773ca74ba6511a99398fdb4a10e0ff0184ab32f3
zhouxindong <zhouxindong@163.com>
2018-10-29 10:54:23 +0800

    Added AdjListNode

---
+++ Z1DataStructAlgorithm/Graph/AdjListNode.cs
@@ -0,0 +1,14 @@
+﻿namespace Z1DataStructAlgorithm.Graph
+{
+    public class AdjListNode<T>
+    {
+        public int AdjVex { get; set; }
+        public AdjListNode<T> Next { get; set; }
+
+        public AdjListNode(int vex)
+        {
+            AdjVex = vex;
+            Next = null;
+        }
+    }
+}
