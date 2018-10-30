38daa1905b271187138530049ee1da17051f6976
zhouxindong <zhouxindong@163.com>
2018-10-30 15:53:30 +0800

    Added GraphAdjList

---
+++ Z1DataStructAlgorithm/Graph/GraphAdjList.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Z1DataStructAlgorithm.Stack;
+
+namespace Z1DataStructAlgorithm.Graph
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class GraphAdjList<T> : IGraph<T>
+    {
+        public VexNode<T>[] AdjList { get; set; }
+
+        private readonly int[] _visited;
+
+        public VexNode<T> this[int index]
+        {
+            get { return AdjList[index]; }
+            set { AdjList[index] = value; }
+        }
+
+        public GraphAdjList(GraphNode<T>[] nodes)
+        {
+            AdjList = new VexNode<T>[nodes.Length];
+            for (var i = 0; i < nodes.Length; i++)
+            {
+                //AdjList[i].Data = nodes[i];
+                //AdjList[i].FirstAdj = null;
+                AdjList[i] = new VexNode<T>(nodes[i]);
+            }
+
+            _visited = new int[AdjList.Length];
+            for (var i = 0; i < _visited.Length; i++)
+            {
+                _visited[i] = 0;
+            }
+        }
+
+        public int GetNumOfVertex()
+        {
+            return AdjList.Length;
+        }
+
+        public int GetNumOfEdge()
+        {
+            int i = 0;
+            foreach (var node in AdjList)
+            {
+                var p = node.FirstAdj;
+                while (p != null)
+                {
+                    ++i;
+                    p = p.Next;
+                }
+            }
+            return i/2;
+        }
+
+        public bool IsNode(GraphNode<T> v)
+        {
+            return AdjList.Any(node => v.Equals(node.Data));
+        }
+
+        public int GetIndex(GraphNode<T> v)
+        {
+            for (var i = 0; i < AdjList.Length; i++)
+            {
+                if (AdjList[i].Data.Equals(v))
+                    return i;
+            }
+
+            return -1;
+        }
+
+        public void SetEdge(GraphNode<T> v1, GraphNode<T> v2, int v)
+        {
+            if (!IsNode(v1) || !IsNode(v2))
+                throw new InvalidOperationException("v1 or v2 not belong to Graph");
+            if (HasEdge(v1, v2))
+                return;
+
+            if (v != 1)
+                throw new InvalidOperationException("Not a Undirection Grapth");
+            var p = new AdjListNode<T>(GetIndex(v2));
+            if ((AdjList[GetIndex(v1)]).FirstAdj == null)
+            {
+                AdjList[GetIndex(v1)].FirstAdj = p;
+            }
+            else
+            {
+                p.Next = AdjList[GetIndex(v1)].FirstAdj;
+                AdjList[GetIndex(v1)].FirstAdj = p;
+            }
+
+            p = new AdjListNode<T>(GetIndex(v1));
+            if (AdjList[GetIndex(v2)].FirstAdj == null)
+            {
+                AdjList[GetIndex(v2)].FirstAdj = p;
+            }
+            else
+            {
+                p.Next = AdjList[GetIndex(v2)].FirstAdj;
+                AdjList[GetIndex(v2)].FirstAdj = p;
+            }
+        }
+
+        public void DelEdge(GraphNode<T> v1, GraphNode<T> v2)
+        {
+            if (!IsNode(v1) || !IsNode(v2))
+                throw new InvalidOperationException("v1 or v2 not belong to Graph");
+            if (!HasEdge(v1, v2))
+                return;
+
+            var p = AdjList[GetIndex(v1)].FirstAdj;
+            AdjListNode<T> pre = null;
+            while (p != null)
+            {
+                if (p.AdjVex != GetIndex(v2))
+                {
+                    pre = p;
+                    p = p.Next;
+                }
+            }
+            pre.Next = p.Next;
+
+            p = AdjList[GetIndex(v2)].FirstAdj;
+            pre = null;
+            while (p != null)
+            {
+                if (p.AdjVex != GetIndex(v1))
+                {
+                    pre = p;
+                    p = p.Next;
+                }
+            }
+
+            pre.Next = p.Next;
+        }
+
+        public bool HasEdge(GraphNode<T> v1, GraphNode<T> v2)
+        {
+            if (!IsNode(v1) || !IsNode(v2))
+                throw new InvalidOperationException("v1 or v2 not belong to Graph");
+
+            var p = AdjList[GetIndex(v1)].FirstAdj;
+            while (p != null)
+            {
+                if (p.AdjVex == GetIndex(v2))
+                {
+                    return true;
+                }
+
+                p = p.Next;
+            }
+            return false;
+        }
+
+        public IEnumerable<VexNode<T>> DeepFirst()
+        {
+            for (int i = 0; i < _visited.Length; i++)
+            {
+                if (_visited[i] == 0)
+                {
+                    foreach (var node in DeepFirst(i))
+                    {
+                        yield return node;
+                    }
+                }
+            }
+        }
+
+        public IEnumerable<VexNode<T>> DeepFirst(int index)
+        {
+            _visited[index] = 1;
+            yield return this[index];
+
+            var p = AdjList[index].FirstAdj;
+            while (p != null)
+            {
+                if (_visited[p.AdjVex] == 0)
+                {
+                    foreach (var node in DeepFirst(p.AdjVex))
+                    {
+                        yield return node;
+                    }
+                }
+
+                p = p.Next;
+            }
+        }
+
+        public IEnumerable<VexNode<T>> BreadthFirst()
+        {
+            for (int i = 0; i < _visited.Length; i++)
+            {
+                if (_visited[i] == 0)
+                {
+                    foreach (var node in BreadthFirst(i))
+                    {
+                        yield return node;
+                    }
+                }
+            }
+        }
+
+        public IEnumerable<VexNode<T>> BreadthFirst(int index)
+        {
+            _visited[index] = 1;
+            yield return this[index];
+            var cq = new CSeqQueue<int>(_visited.Length);
+            cq.In(index);
+            while (!cq.IsEmpty())
+            {
+                int k = cq.Out();
+                var p = AdjList[k].FirstAdj;
+                while (p != null)
+                {
+                    if (_visited[p.AdjVex] == 0)
+                    {
+                        _visited[p.AdjVex] = 1;
+                        yield return this[p.AdjVex];
+                        cq.In(p.AdjVex);
+                    }
+                    p = p.Next;
+                }
+            }
+        }
+    }
+}
---
+++ Z1DataStructAlgorithmTests/Graph/GraphAdjListTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Z1DataStructAlgorithm.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Z1DataStructAlgorithm.Graph.Tests
+{
+    [TestClass()]
+    public class GraphAdjListTests
+    {
+        [TestMethod()]
+        public void DeepFirstTest()
+        {
+            var nodes = new GraphNode<int>[8];
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                nodes[i] = new GraphNode<int>(i + 1);
+            }
+
+            var graph = new GraphAdjList<int>(nodes);
+            graph.SetEdge(nodes[0], nodes[1], 1);
+            graph.SetEdge(nodes[0], nodes[2], 1);
+            graph.SetEdge(nodes[1], nodes[3], 1);
+            graph.SetEdge(nodes[1], nodes[4], 1);
+            graph.SetEdge(nodes[3], nodes[7], 1);
+            graph.SetEdge(nodes[4], nodes[7], 1);
+            graph.SetEdge(nodes[2], nodes[5], 1);
+            graph.SetEdge(nodes[2], nodes[6], 1);
+            graph.SetEdge(nodes[5], nodes[6], 1);
+
+            foreach (var node in graph.DeepFirst())
+            {
+                Console.WriteLine(node.Data.Data);
+            }
+        }
+
+        [TestMethod()]
+        public void BreadthFirstTest()
+        {
+            var nodes = new GraphNode<int>[8];
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                nodes[i] = new GraphNode<int>(i + 1);
+            }
+
+            var graph = new GraphAdjList<int>(nodes);
+            graph.SetEdge(nodes[0], nodes[1], 1);
+            graph.SetEdge(nodes[0], nodes[2], 1);
+            graph.SetEdge(nodes[1], nodes[3], 1);
+            graph.SetEdge(nodes[1], nodes[4], 1);
+            graph.SetEdge(nodes[3], nodes[7], 1);
+            graph.SetEdge(nodes[4], nodes[7], 1);
+            graph.SetEdge(nodes[2], nodes[5], 1);
+            graph.SetEdge(nodes[2], nodes[6], 1);
+            graph.SetEdge(nodes[5], nodes[6], 1);
+
+            foreach (var node in graph.BreadthFirst())
+            {
+                Console.WriteLine(node.Data.Data);
+            }
+        }
+    }
+}
